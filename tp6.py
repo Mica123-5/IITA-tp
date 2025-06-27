c5f6c99e32421a5afff149a12ec8059490fa2c95
Micaela Aramayo <mica.aramayo18@gmail.com>
2025-06-26 23:28:21 -0300

    Version inicial del TP6

---
+++ tp6.py
@@ -0,0 +1,9 @@
+#A Llevar regisstro de datos (npmbre, apellido, fech ncimiento, DNI,
+# Tutor, TodasNotas, cant de faltas, cant. amonestaciones)
+datos= { "Alumnos":[] }
+
+def 
+#B mostrar los datos de c/u
+#C modificar los datos de los alumnos
+#D agregar alumnos
+#E expulsar alumnos
